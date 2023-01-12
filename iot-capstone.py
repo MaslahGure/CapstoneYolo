9aff29a1a02433a311b960c8b969c09f113aa453
Mazlen Zulleikhaa <100333611+Nekoir@users.noreply.github.com>
2023-01-13 03:48:56 +0800

    Update iot-capstone.py

--- iot-capstone.py
+++ iot-capstone.py
@@ -1,5 +1,5 @@
-# Capstone
-# object detection libaries
+##Capstone
+## object detection libaries 
 import requests
 # pip install pillow
 from PIL import Image
@@ -7,13 +7,16 @@ from PIL import Image
 import cv2
 # pip install keyboard
 
+## import the MCP3008 for the soil moisure
+from MCP3008 import MCP3008
+
 
 # extra imports native to python
 import time
 import io
 import base64
 
-# iot libaries
+#iot libaries
 import BlynkLib
 import RPi.GPIO as GPIO
 from BlynkTimer import BlynkTimer
@@ -27,22 +30,26 @@ dhtDevice = adafruit_dht.DHT11(board.D25)
 BLYNK_AUTH_TOKEN = '_EfLT9RJX8GFVrPJ2ou6979syYz1G3Fr'
 blynk = BlynkLib.Blynk(BLYNK_AUTH_TOKEN)
 
-# button for camera detection
+#button for camera detection blynk
 button_value = 0
 button_emptying_fertilizer = 0
 button_emptying_water = 0
+#LED for the button
+LED =26
+#setup led
+GPIO.setup(LED, GPIO.OUT)
 
 tankfull = 4
-tankempty = 9
+tankempty = 14
 
-# motor 1 inputs
-in1 = 6
-in2 = 13
-en = 5
+# motor 1 inputs water
+in1 = 20
+in2 = 21
+en1 = 16
 # motor 2 inputs
-in3 = 20
-in4 = 21
-en2 = 16
+in3 = 6
+in4 = 13
+en2 = 5
 
 
 # ultrasonic 1 inputs, water tank
@@ -51,18 +58,18 @@ ECHO1 = 24
 
 # ultrasonic 2 inputs, fertilizer tank
 TRIG2 = 27
-ECHO2 = 28
+ECHO2 = 22
 
-# soil moisture IO
-smsensor = 4
+#soil moisture IO digital
+#smsensor = 4
 
 # motor 1 inputs setup
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
-GPIO.setup(en, GPIO.OUT)
+GPIO.setup(en1, GPIO.OUT)
 GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
-p1 = GPIO.PWM(en, 1000)
+p1 = GPIO.PWM(en1, 1000)
 p1.start(25)
 
 
@@ -83,11 +90,17 @@ GPIO.setup(ECHO1, GPIO.IN)
 GPIO.setup(TRIG2, GPIO.OUT)
 GPIO.setup(ECHO2, GPIO.IN)
 
-# soil moisture setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(smsensor, GPIO.IN)
-#object detected
-objectDetected =""
+#soil moisture setup digital
+#GPIO.setmode(GPIO.BCM)
+#GPIO.setup(smsensor, GPIO.IN)
+
+#settings for the system
+#tomato #chili
+#defualt
+moistureLowerLimit =0
+moistureUpperLimit =90
+
+objectDetected ="chili"
 
 def get_class_name_confidence():
 
@@ -108,7 +121,7 @@ def get_class_name_confidence():
     time.sleep(1)
 
     # Load image with PIL
-    image = Image.open("tomato_57.jpg").convert("RGB")
+    image =Image.open("tomato_57.jpg").convert("RGB")
 
     # convert to JPEG buffer
     buffer = io.BytesIO()
@@ -160,90 +173,90 @@ def get_class_name_confidence():
 
 # Create BlynkTimer Instance
 timer = BlynkTimer()
-
-
 @blynk.on("connected")
 def blynk_connected():
     print("Raspberry Pi is connected to New Blynk")
 
-
 @blynk.on("V2")
 def v2_write_handler(value):
-    #    global led_switch
+#    global led_switch
     if int(value[0]) != 0:
-        print('Scanner ON')
+        print ('Scanner ON')
         global button_value
-        button_value = 1
+        button_value=1
+        
     else:
         print('Scanner OFF')
-
-        button_value = 0
+        
+        button_value=0
 @blynk.on("V3")
 def v2_write_handler(value):
     #    global led_switch
     if int(value[0]) != 0:
         print('Empty  water tank')
-        global button_value
-        button_value = 1
+        global button_emptying_water
+        button_emptying_water = 1
     else:
         print('Emptying water tank stopped')
-        button_value = 0
+        button_emptying_water = 0
 @blynk.on("V4")
 def v2_write_handler(value):
     #    global led_switch
     if int(value[0]) != 0:
         print('Empty  fertilizer tank')
-        global button_value
-        button_value = 1
+        global button_emptying_fertilizer
+        button_emptying_fertilizer = 1
     else:
         print('Emptying fertilizer tank stopped')
 
-        button_value = 0
-
-
+        button_emptying_fertilizer = 0
+        
 try:
     while True:
+       
         # the fertilizer periodic pump time
         fertilizerTime = time.localtime()
         blynk.run()
         print(button_value)
-
-        # object detection
-        if button_value == 1:
+        
+        #object detection
+        if button_value==1:
             print("Processing pleasing")
+            blynk.virtual_write(5, "Image processing")
+            GPIO.output(LED, GPIO.HIGH)
             result = get_class_name_confidence()
             if (result["confidence"] > 0.5 and result["class_name"] == "Tomato671295"):
+                blynk.virtual_write(5, "Tomato plant detected")
+                blynk.virtual_write(6, "Tomato specification", "Moisture Level > 30")
                 print("tomato detected")
                 objectDetected ="tomato"
+                GPIO.output(LED, GPIO.LOW)
+                moistureLowerLimit =12
+                moistureUpperLimit =95
             elif (result["confidence"] > 0.4 and result["class_name"] == "ChiliPepper677080"):
+                blynk.virtual_write(5, "Chilli plant detected")
+                blynk.virtual_write(6, "Chilli specification", "Moisture Level> 17")
                 print("Chili pepper detected")
-                objectDetected="chili"
+                objectDetected ="chili"
+                GPIO.output(LED, GPIO.LOW)
+                moistureLowerLimit =70
+                moistureUpperLimit =80
             else:
+                blynk.virtual_write(5, "Nothing detected")
                 print("Nothing detected")
-                objectDetected=""
+                GPIO.output(LED, GPIO.LOW)
         #if tomato or chili are detected then run the motors else wait for the next cycle until the button is pressed
         if objectDetected == "tomato" or objectDetected == "chili":
-
-            # humidty and temprature sensor
-            try:
-                # Print the values to the serial port
-                temperature_c = dhtDevice.temperature
-                temperature_f = temperature_c * (9 / 5) + 32
-                humidity = dhtDevice.humidity
-                print(
-                    "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                        temperature_f, temperature_c, humidity
-                    )
-                )
-            except RuntimeError as error:
-                # Errors happen fairly often, DHT's are hard to read, just keep going
-                print(error.args[0])
-                time.sleep(2.0)
-                continue
-            except Exception as error:
-                dhtDevice.exit()
-                raise error
-
+            print(button_emptying_water,button_emptying_fertilizer)
+        
+            #reading moisure
+            adc = MCP3008()
+            value = adc.read( channel = 0 ) # You can of course adapt the channel to be read out
+            moistureInPercent =(100-(value / 1023.0 * 100));
+            
+            print("Percentage Moisture: %.2f" % moistureInPercent )
+            print("")
+            
             # ultrasonic 1 water tank
             # Send a pulse to the sensor
             GPIO.output(TRIG1, True)
@@ -258,71 +271,79 @@ try:
             water_pulse_duration = water_pulse_end - water_pulse_start
             waterLevel = water_pulse_duration * 17150
             waterLevel = round(waterLevel, 2)
-            print("Water level: %.2f cm" % waterLevel)
+            print("Water level:\t\t%.2f cm" % waterLevel)
 
             # ultrasonic 2 fertilizer tank
             # Send a pulse to the sensor
             GPIO.output(TRIG2, True)
             time.sleep(0.00001)
             GPIO.output(TRIG2, False)
+            
             # Measure the time it takes for the pulse to return
             while GPIO.input(ECHO2) == 0:
                 fertilizer_pulse_start = time.time()
+               
             while GPIO.input(ECHO2) == 1:
                 fertilizer_pulse_end = time.time()
             # Calculate the distance based on the time it took for the pulse to return
             fertilizer_pulse_duration = fertilizer_pulse_end - fertilizer_pulse_start
             fertilizerLevel = fertilizer_pulse_duration * 17150
             fertilizerLevel = round(fertilizerLevel, 2)
-            print("Fertilizer level: %.2f cm" % fertilizerLevel)
-
+            print("Fertilizer level:\t%.2f cm" % fertilizerLevel)
+            
             # Motor 1 used to control the water pump, the motor will run if the soil is dry
             # the motor will stop if the soil is wet
             # the motor will stop if the waterLevel is greater than 4cm (Empty water tank1)
-            if (GPIO.input(smsensor)) == 0:
-                print("print wet")
-                print("stop")
+            if (moistureInPercent>=moistureLowerLimit):
+                print("Soil:\t\t\tMOIST")
+                print("Motor:\t\t\tstop")
                 GPIO.output(in1, GPIO.LOW)
                 GPIO.output(in2, GPIO.LOW)
-            elif (GPIO.input(4)) == 1:
-                print("print dry")
-                print("run")
+            elif (moistureInPercent<moistureLowerLimit):
+                print("Soil:\t\t\tDRY")
+                print("Motor:\t\t\trun")
                 if (waterLevel >= tankempty):
                     GPIO.output(in1, GPIO.LOW)
                     GPIO.output(in2, GPIO.LOW)
                     # print low water level
-                    print("low water level")
+                    blynk.virtual_write(5, "Water tank empty!")
+                    print("Water level:\t\tLOW")
                 else:
                     GPIO.output(in1, GPIO.HIGH)
                     GPIO.output(in2, GPIO.LOW)
                     p1.ChangeDutyCycle(30)  # speed of motor
-                    print("forward water pump")
+                    print("Watering soil")
 
-            # Motor 2 used to control the fertilizer pump, the motor will run periodically (every day at 9am)
-            # the motor will stop if the fertilizerLevel is greater than 4cm (Empty fertilizer  tank2)
-            if (fertilizerTime.tm_hour == 9 and fertilizerTime.tm_min == 0 and fertilizerTime.tm_sec == 0):
+             # Motor 2 used to control the fertilizer pump, the motor will run periodically (every day at 9am)
+             # the motor will stop if the fertilizerLevel is greater than 4cm (Empty fertilizer  tank2)
+            #print(fertilizerTime.tm_hour,fertilizerTime.tm_min,fertilizerTime.tm_sec)
+            if (fertilizerTime.tm_hour == 3 and fertilizerTime.tm_min == 35 and fertilizerTime.tm_sec<=20):
                 if (fertilizerLevel >= tankempty):
                     GPIO.output(in3, GPIO.LOW)
                     GPIO.output(in4, GPIO.LOW)
                     # print low fertilizer level
-                    print("low fertilizer level")
-                    # send message to user using blynk
+                    blynk.virtual_write(5, "Fertilizer tank empty!")
+                    print("Fertilizer level:\tLOW")
+                    
+                    #send message to user using blynk
 
                 else:
                     GPIO.output(in3, GPIO.HIGH)
                     GPIO.output(in4, GPIO.LOW)
-                    p1.ChangeDutyCycle(30)  # speed of motor
-                    print("forward fertilizer pump")
+                    p2.ChangeDutyCycle(30)  # speed of motor
+                    print("Fertilizing soil")
+
             else:
                 GPIO.output(in3, GPIO.LOW)
                 GPIO.output(in4, GPIO.LOW)
-                print("stop fertilizer pump")  # stop fertilizer pump
+                print("stop fertilizer pump") # stop fertilizer pump
             #if emptying the water tank is pressed then the motor will run until the water tank is empty
             if button_emptying_water == 1:
                 if (waterLevel >= tankempty):
                     GPIO.output(in1, GPIO.LOW)
                     GPIO.output(in2, GPIO.LOW)
                     # print low water level
+                    blynk.virtual_write(5, "Water tank empty!")
                     print("low water level")
                 else:
                     GPIO.output(in1, GPIO.HIGH)
@@ -338,6 +359,7 @@ try:
                     GPIO.output(in3, GPIO.LOW)
                     GPIO.output(in4, GPIO.LOW)
                     # print low fertilizer level
+                    blynk.virtual_write(5, "Fertilizer tank empty!")
                     print("low fertilizer level")
                 else:
                     GPIO.output(in3, GPIO.HIGH)
@@ -351,7 +373,6 @@ try:
         else:
             print("nothing detected")
             time.sleep(0.5)
-    
 
 finally:
     GPIO.cleanup()
